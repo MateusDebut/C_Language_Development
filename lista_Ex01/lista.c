c126d4c7676692fb6dc0349b8e5d617a78b8649e
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-10-13 23:00:55 -0300

    Atualizações TAD Lista

--- lista_Ex01/lista.c
+++ lista_Ex01/lista.c
@@ -43,6 +43,9 @@ void liberar(lista_t *l){
 void insere(lista_t *l, elem x){
 	assert(l != NULL);
 
+	if (esta_na_lista(l, x) == 1)
+		return;
+
 	no_t *p = (no_t *) malloc(sizeof(no_t));
 	p->info = x;
 	p->prox = NULL;
@@ -73,4 +76,45 @@ void imprimir(lista_t *l){
 		p = p->prox;
 	}
 	printf("\n");
+}
+
+int esta_na_lista(lista_t *l, elem x){
+	assert(l != NULL);
+
+	no_t *p = l->ini;
+	while(p != NULL){
+		if (p->info == x)
+			return 1;
+		p = p->prox;
+	}
+	return 0;
+}
+
+void remove(lista_t *l, elem x){
+	no_t *p = l->ini;
+	no_t *ant = NULL;
+
+	while(p != NULL){
+		if (p->info == x){
+			if (p == l->ini)
+			{
+				l->ini = l->ini->prox;
+				free(p);
+				p = NULL;
+			}else if (p == l->fim)
+			{
+				l->fim = ant;
+				free(p);
+				p = NULL;
+				l->fim->prox = NULL;
+			}else{
+				ant->prox = p->prox;
+				free(p);
+				p = NULL;
+			}
+		}else{
+			ant = p;
+			p = p->prox;
+		}
+	}
 }
--- lista_Ex01/lista.h
+++ lista_Ex01/lista.h
@@ -6,3 +6,5 @@ void liberar(lista_t *l);
 void insere(lista_t *l, elem x);
 int tamanho(lista_t *l);
 void imprimir(lista_t *l);
+int esta_na_lista(lista_t *l, elem x);
+void remove(lista_t *l, elem x);
