a6a527c65d25589c6720db676ac8829fa540a439
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-10-23 22:55:09 -0300

    Merge Sort

--- Merge3Vias/mergeSort.c
+++ Merge3Vias/mergeSort.c
@@ -13,25 +13,57 @@ int main(int argc, char const *argv[])
 	vetor = readVector(stdin, &tamanho);
 	for (int i = 0; i < tamanho; i++)
 	{
-		printf("%d\n", vetor[i]);
+		printf("%d ", vetor[i]);
 	}
+
+	mergeSort(vetor, 0, tamanho);
 
 	return 0;
 }
 
 void mergeSort(int *vetor, int inicio, int fim){
-	if ((fim - inicio) > 1)
-	{
+	if ((fim - inicio) <= 1) return;
 		int meio = (int)((fim + inicio)/2.0);
 		mergeSort(vetor, inicio, meio);
-		mergeSort(vetor, meio, fim);
+		mergeSort(vetor, meio+1, fim);
 		merge(vetor, inicio, meio, fim);
-	}
 	//return vetor;
 }
 
 void merge(int *vetor, int inicio, int meio, int fim){
+	int *aux = (int *) malloc((fim-inicio) * sizeof(int) + 1);
+	int i = inicio;
+	int j = meio+1;
+	int k = 0;
+	
+	while(i <= meio && j <= fim){
+		if (vetor[i] <= vetor[j])
+		{
+			aux[k] = vetor[i];
+			i++;
+		}else{
+			aux[k] = vetor[j];
+			j++;
+		}
+		k++;
+	}
 
+	while(i <= c){
+		aux[k] = v[i];
+		i++; k++;
+	}
+
+		while(j <= fim){
+		aux[k] = v[j];
+		i++; k++;
+	}
+
+	for (i = inicio, k = 0; i <= fim; i++, k++)
+	{
+		vetor[i] = aux[k];
+	}
+
+	free(aux);
 }
 
 int *readVector(FILE *stream, int *i){
