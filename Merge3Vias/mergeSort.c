5c941178ab9a6d72c2b999fecdaf5063b89f0fb3
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-10-25 01:56:49 -0300

    Ordenação funcionando - Merge Sort 3 vias

--- Merge3Vias/mergeSort.c
+++ Merge3Vias/mergeSort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void mergeSort(int *vetor, int inicio, int fim);
-void merge(int *vetor, int inicio, int meio, int fim);
+void merge(int *vetor, int inicio, int meio1, int meio2, int fim);
 int *readVector(FILE *stream, int *i);
 int calcularTamanhoVetor(int *vetor);
 void imprimeVetor(int *vetor, int tamanho);
@@ -13,49 +13,87 @@ int main(int argc, char const *argv[])
 	int tamanho = 0;
 	vetor = readVector(stdin, &tamanho);
 	imprimeVetor(vetor, tamanho);
-	mergeSort(vetor, 0, tamanho-1);
+	mergeSort(vetor, 0, tamanho);
 	imprimeVetor(vetor, tamanho);
 
 	return 0;
 }
 
 void mergeSort(int *vetor, int inicio, int fim){
-	if (fim <= inicio) return;
-		int meio = (int)((fim + inicio)/2.0);
-		mergeSort(vetor, inicio, meio);
-		mergeSort(vetor, meio+1, fim);
-		merge(vetor, inicio, meio, fim);
-	//return vetor;
+	if (fim - inicio < 2) return;
+		int meio1 = inicio + (int)((fim - inicio)/3.0);
+		int meio2 = inicio + 2 * ((int)((fim - inicio)/3.0)) + 1;
+		mergeSort(vetor, inicio, meio1);
+		mergeSort(vetor, meio1, meio2);
+		mergeSort(vetor, meio2, fim);
+		merge(vetor, inicio, meio1, meio2, fim);
 }
 
-void merge(int *vetor, int inicio, int meio, int fim){
+void merge(int *vetor, int inicio, int meio1, int meio2, int fim){
 	int *aux = (int *) malloc((fim-inicio) * sizeof(int) + 1);
 	int i = inicio;
-	int j = meio+1;
-	int k = 0;
-	while(i <= meio && j <= fim){
-		if (vetor[i] <= vetor[j])
+	int j = meio1;
+	int k = meio2;
+	int l = 0;
+	while((i < meio1) && (j < meio2) && (k < fim)){
+		if (vetor[i] < vetor[j])
 		{
-			aux[k] = vetor[i];
-			i++;
+			if (vetor[i] < vetor[k])
+			{
+				aux[l++] = vetor[i++];
+			}else{
+				aux[l++] = vetor[k++];
+			}
 		}else{
-			aux[k] = vetor[j];
-			j++;
+			if (vetor[j] < vetor[k])
+			{
+				aux[l++] = vetor[j++];
+			}else{
+				aux[l++] = vetor[k++];
+			}
 		}
-		k++;
 	}
 
-	while(i <= meio){
-		aux[k] = vetor[i];
-		i++; k++;
+	while((i < meio1) && (j < meio2)){
+		if (vetor[i] < vetor[j])
+		{
+			aux[l++] = vetor[i++];
+		}else{
+			aux[l++] = vetor[j++];
+		}
 	}
 
-		while(j <= fim){
-		aux[k] = vetor[j];
-		j++; k++;
+	while((j < meio2) && (k < fim)){
+		if (vetor[j] < vetor[k])
+		{
+			aux[l++] = vetor[j++];
+		}else{
+			aux[l++] = vetor[k++];
+		}
 	}
 
-	for (i = inicio, k = 0; i <= fim; i++, k++)
+	while((i < meio1) && (k < fim)){
+		if (vetor[i] < vetor[k])
+		{
+			aux[l++] = vetor[i++];
+		}else{
+			aux[l++] = vetor[k++];
+		}
+	}
+
+	while(i < meio1){
+		aux[l++] = vetor[i++];
+	}
+
+	while(j < meio2){
+		aux[l++] = vetor[j++];
+	}
+
+	while(k < fim){
+		aux[l++] = vetor[k++];
+	}
+
+	for (i = inicio, k = 0; i < fim; i++, k++)
 	{
 		vetor[i] = aux[k];
 	}
@@ -74,9 +112,7 @@ int *readVector(FILE *stream, int *i){
 		j = j + 1;
 		*i = *i + 1;
 	}while(!feof(stream));
-	//printf("Esse é o último elemento do vetor: %d\n", vector[j]);
-	vector = (int *) realloc(vector, (j-1) * sizeof(int));
-	//printf("Esse é o último elemento do vetor: %d\n", vector[j-1]);
+	vector = (int *) realloc(vector, (j-2) * sizeof(int));
 	return vector;
 }
 
