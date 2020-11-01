3c53205d0d72e88435942eddaa40df87544de3bf
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-11-01 20:11:19 -0300

    Merge sort de 3 vias finalzado

---
+++ Merge3Vias/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 01/11/2020 14:31:47 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [Exercício 1.2] Merge sort 3-vias
--- Merge3Vias/mergeSort.c
+++ Merge3Vias/mergeSort.c
@@ -1,125 +1,212 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-void mergeSort(int *vetor, int inicio, int fim);
-void merge(int *vetor, int inicio, int meio1, int meio2, int fim);
+#define tamanhoPalavra 256
+
+typedef struct l{
+	char *nomeLivro;
+	float preco;
+}livros_t;
+
+void mergeSort(livros_t *livros, int inicio, int fim);
+void merge(livros_t *livros, int inicio, int div1, int div2, int fim);
 int *readVector(FILE *stream, int *i);
 int calcularTamanhoVetor(int *vetor);
-void imprimeVetor(int *vetor, int tamanho);
+void imprimeVetor(livros_t *livros, int inicio, int fim);
+char *readline(FILE *stream);
+float separaParteNumerica(livros_t livro);
 
 int main(int argc, char const *argv[])
 {
-	int *vetor;
-	int tamanho = 0;
-	vetor = readVector(stdin, &tamanho);
-	imprimeVetor(vetor, tamanho);
-	mergeSort(vetor, 0, tamanho);
-	imprimeVetor(vetor, tamanho);
+	int numeroLivros;
+	scanf(" %d ", &numeroLivros);
+	//getc(stdin);
 
+	livros_t *livros = (livros_t*) malloc(numeroLivros * sizeof(livros_t));
+	for (int i = 0; i < numeroLivros; i++)
+	{
+		livros[i].nomeLivro = readline(stdin);
+		livros[i].preco = separaParteNumerica(livros[i]);
+
+	}
+	mergeSort(livros, 0, numeroLivros);
+	//imprimeVetor(livros, 0,numeroLivros);
+	printf("\n");
+
+	for (int i = 0; i < numeroLivros; i++)
+	{
+		for (int j = 0; j < strlen(livros[i].nomeLivro); j++)
+		{
+			if (livros[i].nomeLivro[j] == '!')
+			{
+				livros[i].nomeLivro[j] = '\0';
+			}
+		}
+		printf("%s\n", livros[i].nomeLivro);
+	}
 	return 0;
 }
 
-void mergeSort(int *vetor, int inicio, int fim){
+
+
+//FUNÇÕES
+void mergeSort(livros_t *livros, int inicio, int fim){
+	imprimeVetor(livros, inicio, fim);
+	if (inicio < fim) printf("\n");
 	if (fim - inicio < 2) return;
-		int meio1 = inicio + (int)((fim - inicio)/3.0);
-		int meio2 = inicio + 2 * ((int)((fim - inicio)/3.0)) + 1;
-		mergeSort(vetor, inicio, meio1);
-		mergeSort(vetor, meio1, meio2);
-		mergeSort(vetor, meio2, fim);
-		merge(vetor, inicio, meio1, meio2, fim);
+		 
+	int div1 = inicio + (int)ceil((fim - inicio)/3.0);
+	int div2 = div1 + (int)round((fim-inicio)/3.0);
+
+	mergeSort(livros, inicio, div1);
+	mergeSort(livros, div1, div2);
+	mergeSort(livros, div2, fim);
+	merge(livros, inicio, div1, div2, fim);		
 }
 
-void merge(int *vetor, int inicio, int meio1, int meio2, int fim){
-	int *aux = (int *) malloc((fim-inicio) * sizeof(int) + 1);
+void merge(livros_t *livros, int inicio, int div1, int div2, int fim){
+	livros_t *aux = (livros_t*)malloc ((fim-inicio) * sizeof(livros_t));
 	int i = inicio;
-	int j = meio1;
-	int k = meio2;
+	int j = div1;
+	int k = div2;
 	int l = 0;
-	while((i < meio1) && (j < meio2) && (k < fim)){
-		if (vetor[i] < vetor[j])
+	while((i < div1) && (j < div2) && (k < fim)){
+		if (livros[i].preco < livros[j].preco)
 		{
-			if (vetor[i] < vetor[k])
+			if (livros[i].preco < livros[k].preco)
 			{
-				aux[l++] = vetor[i++];
+				aux[l].nomeLivro = (char*)malloc(strlen(livros[i].nomeLivro) * sizeof(char));
+				strcpy(aux[l].nomeLivro, livros[i].nomeLivro);
+				aux[l++].preco = livros[i++].preco;
 			}else{
-				aux[l++] = vetor[k++];
+				aux[l].nomeLivro = (char*)malloc(strlen(livros[k].nomeLivro) * sizeof(char));
+				strcpy(aux[l].nomeLivro, livros[k].nomeLivro);
+				aux[l++].preco = livros[k++].preco;
 			}
 		}else{
-			if (vetor[j] < vetor[k])
+			if (livros[j].preco < livros[k].preco)
 			{
-				aux[l++] = vetor[j++];
+				aux[l].nomeLivro = (char*)malloc(strlen(livros[j].nomeLivro) * sizeof(char));
+				strcpy(aux[l].nomeLivro, livros[j].nomeLivro);
+				aux[l++].preco = livros[j++].preco;
 			}else{
-				aux[l++] = vetor[k++];
+				aux[l].nomeLivro = (char*)malloc(strlen(livros[k].nomeLivro) * sizeof(char));
+				strcpy(aux[l].nomeLivro, livros[k].nomeLivro);
+				aux[l++].preco = livros[k++].preco;
 			}
 		}
 	}
 
-	while((i < meio1) && (j < meio2)){
-		if (vetor[i] < vetor[j])
+	while((i < div1) && (j < div2)){
+		if (livros[i].preco < livros[j].preco)
 		{
-			aux[l++] = vetor[i++];
+			aux[l].nomeLivro = (char*)malloc(strlen(livros[i].nomeLivro) * sizeof(char));
+			strcpy(aux[l].nomeLivro, livros[i].nomeLivro);
+			aux[l++].preco = livros[i++].preco;
 		}else{
-			aux[l++] = vetor[j++];
+			aux[l].nomeLivro = (char*)malloc(strlen(livros[j].nomeLivro) * sizeof(char));
+			strcpy(aux[l].nomeLivro, livros[j].nomeLivro);
+			aux[l++].preco = livros[j++].preco;
 		}
 	}
 
-	while((j < meio2) && (k < fim)){
-		if (vetor[j] < vetor[k])
+	while((j < div2) && (k < fim)){
+		if (livros[j].preco < livros[k].preco)
 		{
-			aux[l++] = vetor[j++];
+			aux[l].nomeLivro = (char*)malloc(strlen(livros[j].nomeLivro) * sizeof(char));
+			strcpy(aux[l].nomeLivro, livros[j].nomeLivro);
+			aux[l++].preco = livros[j++].preco;
 		}else{
-			aux[l++] = vetor[k++];
+			aux[l].nomeLivro = (char*)malloc(strlen(livros[k].nomeLivro) * sizeof(char));
+			strcpy(aux[l].nomeLivro, livros[k].nomeLivro);
+			aux[l++].preco = livros[k++].preco;
 		}
 	}
 
-	while((i < meio1) && (k < fim)){
-		if (vetor[i] < vetor[k])
+	while((i < div1) && (k < fim)){
+		if (livros[i].preco < livros[k].preco)
 		{
-			aux[l++] = vetor[i++];
+			aux[l].nomeLivro = (char*)malloc(strlen(livros[i].nomeLivro) * sizeof(char));
+			strcpy(aux[l].nomeLivro, livros[i].nomeLivro);
+			aux[l++].preco = livros[i++].preco;
 		}else{
-			aux[l++] = vetor[k++];
+			aux[l].nomeLivro = (char*)malloc(strlen(livros[k].nomeLivro) * sizeof(char));
+			strcpy(aux[l].nomeLivro, livros[k].nomeLivro);
+			aux[l++].preco = livros[k++].preco;
 		}
 	}
 
-	while(i < meio1){
-		aux[l++] = vetor[i++];
+	while(i < div1){
+		aux[l].nomeLivro = (char*)malloc(strlen(livros[i].nomeLivro) * sizeof(char));
+		strcpy(aux[l].nomeLivro, livros[i].nomeLivro);
+		aux[l++].preco = livros[i++].preco;
 	}
 
-	while(j < meio2){
-		aux[l++] = vetor[j++];
+	while(j < div2){
+		aux[l].nomeLivro = (char*)malloc(strlen(livros[j].nomeLivro) * sizeof(char));
+		strcpy(aux[l].nomeLivro, livros[j].nomeLivro);
+		aux[l++].preco = livros[j++].preco;
 	}
 
 	while(k < fim){
-		aux[l++] = vetor[k++];
+		aux[l].nomeLivro = (char*)malloc(strlen(livros[k].nomeLivro) * sizeof(char));
+		strcpy(aux[l].nomeLivro, livros[k].nomeLivro);
+		aux[l++].preco = livros[k++].preco;
 	}
 
 	for (i = inicio, k = 0; i < fim; i++, k++)
 	{
-		vetor[i] = aux[k];
+		strcpy(livros[i].nomeLivro, aux[k].nomeLivro);
+		livros[i].preco = aux[k].preco;
 	}
 
-	free(aux);
+	//free(aux);
 }
 
-int *readVector(FILE *stream, int *i){
-	*i = 0;
-	int j = 0;
-	int *vector = NULL;
-	
-	do{
-		vector = (int *) realloc(vector, (j+1) * sizeof(int));
-		scanf("%d", &vector[j]);
-		j = j + 1;
-		*i = *i + 1;
-	}while(!feof(stream));
-	vector = (int *) realloc(vector, (j-2) * sizeof(int));
-	return vector;
-}
-
-void imprimeVetor(int *vetor, int tamanho){
-	for (int i = 0; i < tamanho; i++)
+void imprimeVetor(livros_t *livros, int inicio, int fim){
+	for (int i = inicio; i < fim; i++)
 	{
-		printf("%d ", vetor[i]);
+		printf("%.2f ", livros[i].preco);
 	}
-	printf("\n");
+}
+
+char *readline(FILE *stream){
+	int i = 0;
+	char *string = NULL;
+	char aux;
+
+	do{
+		if (i%tamanhoPalavra == 0)
+		{
+			string = (char*)realloc(string, tamanhoPalavra*sizeof(char));
+		}
+		aux = getc(stream);
+		if (aux != '\r')
+		{
+			string[i] = aux;
+		}
+		i++;
+	}while(string[i-1] != '\n' && !feof(stream));
+	string[i-1] = '\0';
+	string = (char*) realloc(string, tamanhoPalavra*sizeof(char));
+	return string;
+}
+
+float separaParteNumerica(livros_t livro){
+	char aux[6];
+	int i = 0;
+	int k = 0;
+	while(livro.nomeLivro[i] != ','){
+		i++;
+	}
+	i++;
+	while(livro.nomeLivro[i] != '\0'){
+		aux[k] = livro.nomeLivro[i];
+		i++;
+		k++;
+	}
+	aux[k] = '\0';
+	return atof(aux);
 }
