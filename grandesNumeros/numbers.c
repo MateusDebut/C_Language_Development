d7668f05948872dd634cf0a34169f2f4f11f62d3
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-11-12 06:40:28 -0300

    criação das funções de inserção e impressão da lista

--- grandesNumeros/numbers.c
+++ grandesNumeros/numbers.c
@@ -24,7 +24,49 @@ list_t *createList(){
 	return pointer;
 }
 
-int putInList(list_t *l, elem){
+
+int putOnList(list_t *l, elem x){
+	assert(l != NULL);
+	node_t *pointer = (node_t *)malloc(sizeof(node_t)); 
+	pointer->info = x;
+	pointer->next = NULL;
+	pointer->previous = NULL;
+
+	node_t *aux;
+	node_t *prev;
+
+	prev = NULL;
+	aux = l->initial;
+
+	while(aux != NULL && x > aux->info){
+		prev = aux;
+		aux = aux->next;
+	}
+
+	//Case where x alredy exist on the list
+	if (aux != NULL && x == aux->info){
+		free(pointer);
+		return 0;
+	}
+
+	//In which case x must be inserted in the starting position (smaller element or empty list)
+	if (prev == NULL)
+	{
+		pointer->next = l->initial;
+		if (l->initial != NULL)
+			l->initial->previous = pointer;
+		l->initial = pointer;
+	}
+	//In which case x will be inserted on the middle or in the final of the list 
+	else{
+		pointer->next = prev->next;
+		prev->next = pointer;
+		if (pointer->next != NULL)
+			pointer->next->previous = pointer;
+		else
+			l->final = pointer;
+		pointer->previous = prev;
+	}
 
 }
 
@@ -32,7 +74,8 @@ int removeFromList(list_t *l, elem x){
 
 }
 
-int printList(list_t *l){
+void printList(list_t *l){
+	assert(l != NULL);
 	node_t *pointer = l->initial;
 	while(pointer != NULL){
 		printf("%d ", pointer->info);
@@ -41,7 +84,15 @@ int printList(list_t *l){
 	printf("\n");
 }
 
-int printListBackwards(list_t *l){
+void printListBackwards(list_t *l){
+
+	assert(l != NULL);
+	node_t *pointer = l->final;
+	while(pointer != NULL){
+		printf("%d ", pointer->info);
+		pointer = pointer->previous;
+	}
+	printf("\n");
 
 }
 
--- grandesNumeros/numbers.h
+++ grandesNumeros/numbers.h
@@ -2,8 +2,8 @@ typedef int elem;
 typedef stuct list list_t;
 
 list_t *createList();
-int putInList(list_t *l, elem);
+int putOnList(list_t *l, elem x);
 int removeFromList(list_t *l, elem x);
-int printList(list_t *l);
-int printListBackwards(list_t *l);
+void printList(list_t *l);
+void printListBackwards(list_t *l);
 void destroyList(list_t *l);
