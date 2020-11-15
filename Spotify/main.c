0b1ceebc3497f8bd4badc3295951583717e88ab8
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2020-11-15 16:04:25 -0300

    alterações na main do projeto Spotify

--- Spotify/main.c
+++ Spotify/main.c
@@ -7,23 +7,25 @@ int main(int argc, char const *argv[])
 {
 	char *nomeArquivo;
 	nomeArquivo = readline(stdin);
+
 	dados_t *dados = NULL;
 	FILE *fp;
 	fp = fopen(nomeArquivo, "r");
-	int i = 0;
+	int numero_linhas = 0;
 	header_t header;
+	header = criaStructHeader(header, fp);
 
 	while(!feof(fp)){
-		if (i==0)
-		{
-			header = criaStructHeader(header, fp);
-		}else{
-			dados = (dados_t*) realloc(dados, (i+1)*sizeof(dados_t));
-			dados[i] = criaStructDados(dados[i], fp);
-		}
-		i++;
-		printf("(%i)\n", i);
+		dados = (dados_t*) realloc(dados, (numero_linhas+1)*sizeof(dados_t));
+		dados[numero_linhas] = criaStructDados(dados[numero_linhas], fp);
+		numero_linhas++;
 	}
+
+	numero_linhas--;
+	qsort(dados, numero_linhas, sizeof(dados_t), compararNome);
+	posicao_t posicao;
+	posicao = contaArtista(dados, numero_linhas, posicao);
+
 	fclose(fp);
 	return 0;
 }
